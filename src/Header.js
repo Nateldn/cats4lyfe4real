e674177b869f2cd21e5c4c79bcf745ac6d5e1688
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-02-17 12:33:12 +0000

    Add React-Bootstrap

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ const Header = () =>{
     return(
         <div className='header'>
             <img id='logo' src={logo2}/>
-            <h1 id='title'>Cats-4-Life</h1>
+            <h1 id='title'>Cats-4-Life-4-Real</h1>
             <div className='links'>
                 <p>Home</p>
                 <p>Products</p>
